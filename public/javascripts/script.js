f231271096ba668fb2e7a5c1eac9afa73db3a2a6
bjacquemet <baptiste.jacquemet@fabernovel.com>
2015-07-22 16:51:46 +0200

    add chart to volunteer profile

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,3 +8,25 @@ $(document).ready(function(){
           return this.href == url;
       }).parent().addClass('active');
 });
+$(document).ready(function(){
+Chart.defaults.global.responsive = false;
+var data = [
+{
+  value: 35,
+  color:"#666",
+  highlight: "#444",
+  label: "% Completed"
+},
+{
+  value: 65,
+  color: "rgba(240, 240, 240, 0.8)",
+  highlight: "rgba(240, 240, 240, 0.6)",
+  label: "% remaining"
+}
+]
+window.onload = function()
+  {
+    var happiness = $('#goals').get(0).getContext("2d");
+    var newChart = new Chart(happiness).Doughnut(data, {animationEasing : "easeInQuad", animationSteps : 50,   tooltipTemplate: "<%=value%><%if(label){%><%=label%><%}%>"});
+  }
+});
