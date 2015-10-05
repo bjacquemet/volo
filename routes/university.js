2eb77f8f55e667208bf80661d03a6e899491e572
bjacquemet <baptiste.jacquemet@fabernovel.com>
2015-10-05 11:24:05 +0200

    ensure admin is logged to create new uni right

--- routes/university.js
+++ routes/university.js
@@ -3,12 +3,23 @@ var router = express.Router();
 var TrackingController = require('../controllers/tracking');
 var UniversityController = require('../controllers/university');
 
+function ensureAdmin(req, res, next) {
+  if (req.user && req.user.usertype.indexOf('admin') > 0) { return next(); }
+  res.sendStatus(401);
+}
+
+function ensureAuthenticated(req, res, next) {
+  if (req.user) { return next(); }
+  res.sendStatus(401);
+}
+
 router.post("/new", UniversityController.new);
 
 router.get("/list", UniversityController.list);
 
+router.post('/newRight', ensureAdmin, TrackingController.new);
+
 // must be the last one in order to not overwrite the other ones
 router.get('/:university', TrackingController.all);
-router.post('/newRight', TrackingController.new);
 
 module.exports = router;
